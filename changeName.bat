41bb0f4d5bb16f8ce4ff0d542ce838964bc9b866
FCD 寮犱簯宄? <994318935@qq.com>
2020-02-21 10:33:41 +0800

    basic

---
+++ changeName.bat
@@ -0,0 +1,8 @@
+git config user.name
+git config user.email
+
+git config --global user.name "LANA"
+git config --global user.email "LANA@LANA.COM"
+
+git config user.name
+git config user.email
---
+++ reChangeName.bat
@@ -0,0 +1,8 @@
+git config user.name
+git config user.email
+
+git config --global user.name "FCD 张云峰"
+git config --global user.email "994318935@qq.com"
+
+git config user.name
+git config user.email
